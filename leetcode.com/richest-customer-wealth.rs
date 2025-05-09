9fc89b29999da6a0a373094d50e9fc3fd8036d97
floaterest <floaterest@users.noreply.github.com>
2025-05-09 15:07:37 -0400

    add(leetcode.com) richest-customer-wealth.rs
    https://leetcode.com/problems/richest-customer-wealth

---
+++ leetcode.com/richest-customer-wealth.rs
@@ -0,0 +1,19 @@
+// https://leetcode.com/problems/richest-customer-wealth
+// https://leetcode.com/problems/running-sum-of-1d-array/
+pub struct Solution;
+
+impl Solution {
+    pub fn maximum_wealth(accounts: Vec<Vec<i32>>) -> i32 {
+        accounts.iter().map(|v|v.iter().sum()).max().unwrap()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_1480() {
+        assert_eq!(Solution::maximum_wealth(vec![vec![2, 8, 7], vec![7, 1, 3], vec![1, 9, 5]]), 17);
+    }
+}
