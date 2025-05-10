7e432aec7dcbc1f72bbd1f3b94e057f36a51b696
floaterest <floaterest@users.noreply.github.com>
2025-05-10 16:44:20 -0400

    update(leetcode.com) number-of-steps-to-reduce-a-number-to-zero.rs
    https://leetcode.com/problems/number-of-steps-to-reduce-a-number-to-zero/

--- leetcode.com/number-of-steps-to-reduce-a-number-to-zero.rs
+++ leetcode.com/number-of-steps-to-reduce-a-number-to-zero.rs
@@ -3,8 +3,12 @@ pub struct Solution;
 
 impl Solution {
     pub fn number_of_steps(num: i32) -> i32 {
-        let s = format!("{num:b}");
-        (s.len() + s.chars().filter(|&c| c == '1').count()) as i32 - 1
+        if num == 0 {
+            return 0;
+        }
+        let a = num.ilog2() as i32;
+        let b = format!("{num:b}").chars().filter(|&c| c == '1').count() as i32;
+        a + b
     }
 }
 
