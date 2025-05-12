9de8b13e4467df1e4a03ab74368458bbaec2f994
floaterest <floaterest@users.noreply.github.com>
2025-05-12 16:20:05 -0400

    update(leetcode.com) valid-parentheses.rs (refactor)
    https://leetcode.com/problems/valid-parentheses/

--- leetcode.com/valid-parentheses.rs
+++ leetcode.com/valid-parentheses.rs
@@ -4,21 +4,17 @@ pub struct Solution;
 impl Solution {
     pub fn is_valid(s: String) -> bool {
         let mut v = vec![];
+        let pair = |a| match a {
+            '(' => ')',
+            '[' => ']',
+            '{' => '}',
+            _ => unreachable!(),
+        };
         for ch in s.chars() {
             match ch {
                 '(' | '[' | '{' => v.push(ch),
                 _ => {
-                    if let Some(a) = v.pop() {
-                        let b = match a {
-                            '(' => ')',
-                            '[' => ']',
-                            '{' => '}',
-                            _ => unreachable!(),
-                        };
-                        if b != ch {
-                            return false;
-                        }
-                    } else {
+                    if v.pop().map(|a| pair(a) != ch).unwrap_or(true) {
                         return false;
                     }
                 }
