2bd5bbce5404fd9bb76442c19394547559b00e26
floaterest <floaterest@users.noreply.github.com>
2025-05-13 16:01:06 -0400

    update(leetcode.com) house-robber.rs
    https://leetcode.com/problems/house-robber/

--- leetcode.com/house-robber.rs
+++ leetcode.com/house-robber.rs
@@ -3,23 +3,17 @@ pub struct Solution;
 
 impl Solution {
     pub fn rob(nums: Vec<i32>) -> i32 {
-        let n = nums.len();
         // assume items are numbered 1, 2, ...
         // dp[i] is best rob for items 1..i
 
         // dp[i] is either
         //   dp[i - 2] + v_i if take the ith item
         //   dp[i - 1]       if skip the ith item
-        let mut dp = Vec::with_capacity(n);
-        dp.push(0);
-        dp.push(0);
-        let mut u = 0;
+        let (mut a1, mut a2) = (0, 0);
         for v in nums.iter() {
-            let i = dp.len();
-            u = dp[i - 1].max(dp[i - 2] + v);
-            dp.push(u);
+            (a2, a1) = (a1, a1.max(a2 + v));
         }
-        u
+        a1
     }
 }
 
