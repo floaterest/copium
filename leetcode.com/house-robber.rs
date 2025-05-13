a38ba275c8132c8c937f9648c889e99a746f33a3
floaterest <floaterest@users.noreply.github.com>
2025-05-13 16:07:50 -0400

    update(leetcode.com) house-robber.rs
    https://leetcode.com/problems/house-robber/

--- leetcode.com/house-robber.rs
+++ leetcode.com/house-robber.rs
@@ -5,15 +5,10 @@ impl Solution {
     pub fn rob(nums: Vec<i32>) -> i32 {
         // assume items are numbered 1, 2, ...
         // dp[i] is best rob for items 1..i
-
         // dp[i] is either
         //   dp[i - 2] + v_i if take the ith item
         //   dp[i - 1]       if skip the ith item
-        let (mut a1, mut a2) = (0, 0);
-        for v in nums.iter() {
-            (a2, a1) = (a1, a1.max(a2 + v));
-        }
-        a1
+        nums.iter().fold((0, 0), |(a1, a2), v| (a1.max(a2 + v), a1)).0
     }
 }
 
