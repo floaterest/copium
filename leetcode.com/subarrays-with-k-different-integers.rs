56f3a411613daccd94b07f03ffabdca65c8ef9e4
floaterest <floaterest@users.noreply.github.com>
2025-05-17 14:14:39 -0400

    update(leetcode.com) subarrays-with-k-different-integers

--- leetcode.com/subarrays-with-k-different-integers.rs
+++ leetcode.com/subarrays-with-k-different-integers.rs
@@ -3,26 +3,27 @@ use std::collections::HashMap;
 impl Solution {
     pub fn subarrays_with_k_distinct(nums: Vec<i32>, k: i32) -> i32 {
         // O(n) sliding window
-        let mut h = HashMap::new();
-        let (mut i, mut m, k) = (0, 0, k as usize);
-        let mut total = 0;
-        for a in &nums {
-            *h.entry(*a).or_insert(0) += 1;
-            if h.len() > k {
-                assert_eq!(h.len(), k + 1);
-                assert_eq!(h.remove(&nums[m]).unwrap(), 1);
-                m += 1;
-                i = m;
-            }
-            if h.len() == k {
-                while h[&nums[m]] > 1 {
-                    h.entry(nums[m]).and_modify(|e| *e -= 1);
-                    m += 1;
+        let k = k as usize;
+        nums.iter()
+            .scan((0, 0, HashMap::new()), |(i, m, h), a| {
+                *h.entry(*a).or_insert(0) += 1;
+                if h.len() > k {
+                    assert_eq!(h.len(), k + 1);
+                    assert_eq!(h.remove(&nums[*m]).unwrap(), 1);
+                    *m += 1;
+                    *i = *m;
                 }
-                total += m - i + 1;
-            }
-        }
-        total as i32
+                if h.len() == k {
+                    while h[&nums[*m]] > 1 {
+                        h.entry(nums[*m]).and_modify(|e| *e -= 1);
+                        *m += 1;
+                    }
+                    Some(*m - *i + 1)
+                } else {
+                    Some(0)
+                }
+            })
+            .sum::<usize>() as i32
     }
 }
 
