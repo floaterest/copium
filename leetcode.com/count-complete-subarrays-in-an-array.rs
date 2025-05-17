f91425daf82b1a48e51fa287da45930198469c08
floaterest <floaterest@users.noreply.github.com>
2025-05-17 13:36:14 -0400

    update(leetcode.com) count-complete-subarrays-in-an-array

--- leetcode.com/count-complete-subarrays-in-an-array.rs
+++ leetcode.com/count-complete-subarrays-in-an-array.rs
@@ -4,27 +4,27 @@ impl Solution {
     pub fn count_complete_subarrays(nums: Vec<i32>) -> i32 {
         let n = nums.len(); // >= 1
         let k = nums.iter().collect::<HashSet<_>>().len();
-        let (mut l, mut r, mut m) = (0, 0, 0);
+        // sliding window, O(n) time
+        let (mut l, mut r, mut h, mut m) = (0, 0, HashMap::new(), 0);
         // loop invariant:
-        // l <= r
-        // h is frequency count of nums[l..r]
-        let mut h: HashMap<i32, usize> = HashMap::new();
-        while r <= n {
-            if h.len() >= k {
-                // [l..r], [l..r+1], ..., [l..n] are valid
+        // - l <= r
+        // - h is freq count on nums[l..r]
+        while l < n && r <= n {
+            if h.len() < k {
+                // not enough distinct elements => try next r
+                if let Some(&x) = nums.get(r) {
+                    *h.entry(x).or_insert(0) += 1;
+                }
+                r += 1;
+            } else {
+                // [l..r], [l..r+1], ..., [l..n] are all valid
                 m += n - r + 1;
-                // move left endpoint
+                // try next l
                 h.entry(nums[l]).and_modify(|e| *e -= 1);
                 if h[&nums[l]] == 0 {
                     h.remove(&nums[l]);
                 }
                 l += 1;
-            } else {
-                // not enough distinct elements in [l..r]
-                if r < n {
-                    *h.entry(nums[r]).or_insert(0) += 1;
-                }
-                r += 1;
             }
         }
         m as i32
