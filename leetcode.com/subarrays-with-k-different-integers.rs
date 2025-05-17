30ea41630be5aa3d12e7d1425aa55ec171c38578
floaterest <floaterest@users.noreply.github.com>
2025-05-17 13:46:55 -0400

    add(leetcode.com) subarrays-with-k-different-integers

---
+++ leetcode.com/subarrays-with-k-different-integers.rs
@@ -0,0 +1,35 @@
+// https://leetcode.com/problems/subarrays-with-k-different-integers/
+use std::collections::HashMap;
+impl Solution {
+    pub fn subarrays_with_k_distinct(nums: Vec<i32>, k: i32) -> i32 {
+        // O(n) sliding window
+        let mut h = HashMap::new();
+        let (mut i, mut m, k) = (0, 0, k as usize);
+        let mut total = 0;
+        for a in &nums {
+            *h.entry(*a).or_insert(0) += 1;
+            if h.len() > k {
+                assert_eq!(h.len(), k + 1);
+                assert_eq!(h.remove(&nums[m]).unwrap(), 1);
+                m += 1;
+                i = m;
+            }
+            if h.len() == k {
+                while h[&nums[m]] > 1 {
+                    h.entry(nums[m]).and_modify(|e| *e -= 1);
+                    m += 1;
+                }
+                total += m - i + 1;
+            }
+        }
+        total as i32
+    }
+}
+
+pub struct Solution;
+
+fn main() {
+    assert_eq!(Solution::subarrays_with_k_distinct(vec![1, 2, 1, 2, 3], 2), 7);
+    assert_eq!(Solution::subarrays_with_k_distinct(vec![1, 2, 1, 3, 4], 3), 3);
+    println!("test result: ok.");
+}
