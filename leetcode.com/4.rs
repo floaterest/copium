8ec0129199ea8a0436b11942bf8467eaa2bb5888
floaterest <floaterest@users.noreply.github.com>
2025-05-22 16:09:05 -0400

    update(leetcode.com) median-of-two-sorted-arrays

--- leetcode.com/4.rs
+++ leetcode.com/4.rs
@@ -2,15 +2,15 @@
 impl Solution {
     pub fn find_median_sorted_arrays(ns1: Vec<i32>, ns2: Vec<i32>) -> f64 {
         let (n1, n2) = (ns1.len(), ns2.len());
-        if n1 < n2 {
+        if n1 > n2 {
             return Self::find_median_sorted_arrays(ns2, ns1);
         }
-        // binary search on 2m+1
-        let (mut i, mut j) = (0, n2 * 2);
+        // binary search on 2n+1
+        let (mut i, mut j) = (0, n1 * 2);
         while i <= j {
-            // try cut at B (with its corresponding cut at A)
-            let m2 = (i + j) / 2;
-            let m1 = n1 + n2 - m2;
+            // try cut at nums1 (with its corresponding cut at nums2)
+            let m1 = (i + j) / 2;
+            let m2 = n1 + n2 - m1;
             // compute endpoints
             let l1 = m1.checked_sub(1).and_then(|i| ns1.get(i / 2).copied()).unwrap_or(i32::MIN);
             let l2 = m2.checked_sub(1).and_then(|i| ns2.get(i / 2).copied()).unwrap_or(i32::MIN);
@@ -18,9 +18,9 @@ impl Solution {
             let r2 = ns2.get(m2 / 2).copied().unwrap_or(i32::MAX);
             // binary search
             if l1 > r2 {
-                i = m2 + 1;
+                j = m1 - 1;
             } else if l2 > r1 {
-                j = m2 - 1;
+                i = m1 + 1;
             } else {
                 return (l1.max(l2) + r1.min(r2)) as f64 / 2.0;
             }
