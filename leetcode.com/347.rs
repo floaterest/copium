15a53d36208230698c326bc7425c43b4bf7d4a17
floaterest <floaterest@users.noreply.github.com>
2025-05-25 13:49:45 -0400

    update(leetcode.com) top-k-frequent-elements simpler?

--- leetcode.com/347.rs
+++ leetcode.com/347.rs
@@ -1,36 +1,21 @@
 // https://leetcode.com/problems/top-k-frequent-elements/
-use std::collections::HashMap;
+use std::collections::{BinaryHeap, HashMap};
 impl Solution {
     pub fn top_k_frequent(nums: Vec<i32>, k: i32) -> Vec<i32> {
-        // O(n) time and space
-        let n = nums.len();
-        // num -> frequency
-        nums.into_iter()
-            .fold(HashMap::new(), |mut h, a| {
-                *h.entry(a).or_insert(0) += 1;
-                h
-            })
-            // frequency -> nums
-            .into_iter()
-            .fold(vec![vec![]; n + 1], |mut v, (a, freq)| {
-                v[n - freq].push(a);
-                v
-            })
-            // 1D array of nums (most frequent first)
-            .into_iter()
-            .flatten()
+        let h = nums.into_iter().fold(HashMap::new(), |mut h, x| {
+            *h.entry(x).or_insert(0) += 1;
+            h
+        });
+        let mut h: BinaryHeap<_> = h.into_iter().map(|(x, c)| (c, x)).collect();
+        std::iter::from_fn(|| h.pop())
             .take(k as usize)
+            .map(|(_, x)| x)
             .collect()
     }
 }
 
-pub struct Solution;
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    #[test]
-    fn t() {
-        assert_eq!(Solution::top_k_frequent(vec![1, 1, 1, 2, 2, 3], 2), vec![1, 2]);
-    }
+struct Solution;
+fn main() {
+    assert_eq!(Solution::top_k_frequent(vec![1, 1, 1, 2, 2, 3], 2), vec![1, 2]);
+    println!("test result: ok.");
 }
